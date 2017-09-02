6fb82c224522c58f27befd383fc91f8735296940
Esterling Accime <esterling@brainbytescreative.com>
2017-09-02 13:17:35 -0400

    Firebase Section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,100 +1,223 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyANdgJlHSYjgnSAeWOkGqfVkOBtemYbncw",
+    authDomain: "semistackapp.firebaseapp.com",
+    databaseURL: "https://semistackapp.firebaseio.com",
+    projectId: "semistackapp",
+    storageBucket: "semistackapp.appspot.com",
+    messagingSenderId: "782966338001"
+  };
+  firebase.initializeApp(config);
 
-var endpoint = 'http://api.wunderground.com/api/067abeafb1e98bba/conditions/';
-
-
-var parameters = 'q/GA/roswell.json';
-
-
-
-$.ajax({
-	url:endpoint + parameters,
-	method:'GET'
-})
-.done(function (data) {
-
-	var f = data.current_observation.temp_f,
-
-		c = data.current_observation.temp_c,
-
-		icon = data.current_observation.icon_url,
-
-		status = data.current_observation.observation_time,
-
-		wind_moving = data.current_observation.wind_string,
-
-		weather_report = data.current_observation.weather,
-
-		location = data.current_observation.display_location;
+// Grabbing Firebase Services
+var auth = firebase.auth(),
+	database = firebase.database();
 
 
-	var toggle = false;
+// Sending the User to the sign up html 
+$("#signup").on('click', function () {
+	location.href = "signup.html";
+});
 
 
-	var name = location.city + ', ' + location.state + ', ' + location.zip;
-
-		$("h2").html(name);
-		$("#f").html("Temparature: " + f + ' F');
-		$("img").attr('src', icon);
-		$("#condition").html(weather_report);
-		$("#status").html(status);
-
-	// $("#loading").html("Data loading...");
-
-	// setTimeout(function () {
-		
-
-	// 	$("#loading").empty();
-	// }, 4000);
-
-
-	$("#convert").on('click', function () {
-		
-		if(toggle === false){
-
-			$("#f").html("Temparature: " + f + ' F');
-			$("#convert").html("Convert into C");
-			toggle = true;
-
-		} else{
-
-			$("#f").html("Temparature: " + c + ' C');
-			$("#convert").html("Convert into F");
-
-			toggle = false;
-			
-		}
-	});
-
-
-
-
-
-
-	console.group();
-		console.log(data);
-		console.log("Tempature: ", data.current_observation.temp_f, " F");
-		console.log("Tempature: ", data.current_observation.temp_c, " C");
-		console.log("Tempature: ", data.current_observation.icon_url, 'Icon');
-		console.log("Last updated: ", data.current_observation.observation_time, 'last updated');
-		console.log("Wind stable: ", data.current_observation.wind_string, 'stable');
-		console.log("weather condition: ", data.current_observation.weather, 'weather condition');
-		console.log("Location: ", data.current_observation.display_location, 'Location');
-	console.groupEnd();
-
-})
-.catch(function (err) {
-
-	$(".weather").html("Oopps, sorry, the server is down.");
+// Sending the User to the sign in html 
+$("#signin").on('click', function () {
+	location.href = "signin.html";
 
 });
 
 
-// <h1>Weather condition in Roswell, GA, 30076</h1>
-// 			<p>Temparature: 30 F</p>
-// 			<button>Convert into C</button>
-// 			<i>Icons over here</i>
-// 			<p>Weather Condition:  Mostly Cloudy weather condition</p>
+// Sign the user up
+$("#signUserUp").on('click', function (event) {
 
-// 			<p>Last updated: Last Updated on August 26, 8:58 PM EDT last updated</p>
+	event.preventDefault();
+
+
+	// Grabbing the input values
+	var firstname = $("#first_name").val().trim(),
+		lastname = $("#last_name").val().trim(),
+		password = $("#password").val().trim(),
+		email = $("#email").val().trim();
+
+
+
+	// Firebase methods to sign the user up
+	auth.createUserWithEmailAndPassword(email, password) // promise
+		.then(function (newUser) {
+
+			var user = {
+				firstname:firstname,
+				lastname:lastname,
+				password:password,
+				email:email,
+				id:newUser.uid
+			};	
+
+
+			var ref = database.ref('users/' + newUser.uid); // refering to our database
+
+				ref.set(user); // save the data into the database
+
+			location.href = "dashboard.html"; // dashboard for logged in users
+
+		}) 
+		// Dealing with errors
+		.catch(function (err) {
+			console.log(err);
+			console.log("Noooo, something went wrong");
+		}); 
+
+
+	// Clearing the input values
+	$("#first_name").val("");
+	$("#last_name").val("");
+	$("#password").val("");
+	$("#email").val("");
+
+});
+
+
+
+$('#signinDash').on('click', function (event) {
+
+	event.preventDefault();
+
+	var password = $("#passwordSign").val().trim(),
+		email = $("#emailSign").val().trim();
+
+	console.log(password, email);	
+
+	auth.signInWithEmailAndPassword(email, password)	
+		.then(function (user) {
+			console.log(user);
+
+			location.href = "dashboard.html";
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+
+});
+
+// Sign the logged in user out
+$("#signout").on('click', function () {
+
+	auth.signOut() // firebase method to sign otu
+		.then(function () {
+
+			location.href = 'signin.html'; // redirect the user to the sign in page
+			
+		})
+		// Dealing with errors
+		.catch(function (err) {
+			console.log(err);
+		})
+
+});
+
+// Dashboard section
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+   
+   	// Refering the Firebase Section
+    var refLanguage = database.ref("users/" + user.uid + "/languages"),
+    	ref = database.ref("users/" + user.uid),
+    	allUsers = database.ref("users");
+
+
+    // Get the current logged in user info
+    ref.on('value', function (user) {
+
+    	var currentUser = user.val(); // info
+
+    	$("#fullname").html("Welcome back, dear " +  currentUser.firstname + " " + currentUser.lastname);
+    	$("#email").html(currentUser.email);
+
+    });
+
+    // Adding languages to the Database
+    $("#addLanguage").on('click', function (event) {
+    	
+    	event.preventDefault(); // preventing the browser from refreshing
+
+    	var language = $("#language").val().trim();
+
+    	var userLanguage = {
+
+    		language:language
+    	}
+
+    	refLanguage.set(userLanguage); // firebase reference
+
+    });
+
+
+    // Getting the languages values
+    refLanguage.on("value", function (languages) {
+
+    	var currentUserLanguage = languages.val();
+
+    	// Output to the DOM
+    	$("#language").val(currentUserLanguage.language);
+    	$("#display").html(currentUserLanguage.language);
+    });
+
+
+
+    // Display all users
+    allUsers.on('value', function (all) {
+    	var allCurrentUser = all.val();
+
+    	console.log(allCurrentUser);
+
+    	var displayAllUsers = [];
+
+
+    	// Looping through users ID
+    	for(var user in allCurrentUser){
+    		displayAllUsers.push("User: " + allCurrentUser[user].firstname + ", ");
+    		
+    	}
+
+    	// Looping through all users
+    	for(var i = 0; i < displayAllUsers.length; i++){
+    		var li = $("<li>");
+    			li.html(displayAllUsers[i]);
+
+    		$("#users").append(li);	// appending them to the ul tag in the HTML
+    	}
+    });
+
+  } else {
+
+   	console.log("No user found!!!");
+
+  }
+});
+
+
+// Note: 
+// The way, we are displaying all the users, is not recommended for security reasons
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
