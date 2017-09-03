5edb02cb4a894b94dbb099deea1e5dd8ddf92756
Esterling Accime <esterling@brainbytescreative.com>
2017-09-03 17:20:29 -0400

    Twitch TV APP

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,203 +1,180 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyANdgJlHSYjgnSAeWOkGqfVkOBtemYbncw",
-    authDomain: "semistackapp.firebaseapp.com",
-    databaseURL: "https://semistackapp.firebaseio.com",
-    projectId: "semistackapp",
-    storageBucket: "semistackapp.appspot.com",
-    messagingSenderId: "782966338001"
-  };
-  firebase.initializeApp(config);
+// Twitch TV App
+var users = [
+    "ESL_SC2", 
+    "OgamingSC2", 
+    "cretetion", 
+    "freecodecamp", 
+    "storbeck", 
+    "habathcx", 
+    "RobotCaleb", 
+    "noobs2ninjas",
+    "accimeesterlin"
+];
+var userEndPoint = 'https://wind-bow.glitch.me/twitch-api/users/',
+    userParams = 'accimeesterlin';
 
-// Grabbing Firebase Services
-var auth = firebase.auth(),
-	database = firebase.database();
+var channelEndPoint = 'https://wind-bow.glitch.me/twitch-api/channels/',
+    channelParams = 'accimeesterlin';
 
+// Data about users currently streaming
+var streamEndpoint = 'https://wind-bow.glitch.me/twitch-api/streams/',
+    streamParams = 'OgamingSC2';
 
-// Sending the User to the sign up html 
-$("#signup").on('click', function () {
-	location.href = "signup.html";
-});
+var getData = function (endpoint, params, callback) {
+    // Get data from api
+    $.ajax({
+        url: endpoint + params,
+        method:'GET'
+    })
+    .then(function (data) {
+        callback(data);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+};
 
+// Users data
+for(var i = 0; i < users.length; i++){
+    getData(userEndPoint, users[i], function (user) {
+        var usersContainer = $("<div>");
+            usersContainer.attr({
+                "class":"users offline " + user.display_name,
+                "id": user._id
+            });
 
-// Sending the User to the sign in html 
-$("#signin").on('click', function () {
-	location.href = "signin.html";
+        var title = $("<h2>");
+            title.html(user.display_name);
 
-});
+        var img = $("<img>");
+            img.attr({
+                "src":user.logo
+            });
 
-
-// Sign the user up
-$("#signUserUp").on('click', function (event) {
-
-	event.preventDefault();
-
-
-	// Grabbing the input values
-	var firstname = $("#first_name").val().trim(),
-		lastname = $("#last_name").val().trim(),
-		password = $("#password").val().trim(),
-		email = $("#email").val().trim();
-
-
-
-	// Firebase methods to sign the user up
-	auth.createUserWithEmailAndPassword(email, password) // promise
-		.then(function (newUser) {
-
-			var user = {
-				firstname:firstname,
-				lastname:lastname,
-				password:password,
-				email:email,
-				id:newUser.uid
-			};	
-
-
-			var ref = database.ref('users/' + newUser.uid); // refering to our database
-
-				ref.set(user); // save the data into the database
-
-			location.href = "dashboard.html"; // dashboard for logged in users
-
-		}) 
-		// Dealing with errors
-		.catch(function (err) {
-			console.log(err);
-			console.log("Noooo, something went wrong");
-		}); 
-
-
-	// Clearing the input values
-	$("#first_name").val("");
-	$("#last_name").val("");
-	$("#password").val("");
-	$("#email").val("");
-
-});
-
-
-
-$('#signinDash').on('click', function (event) {
-
-	event.preventDefault();
-
-	var password = $("#passwordSign").val().trim(),
-		email = $("#emailSign").val().trim();
-
-	console.log(password, email);	
-
-	auth.signInWithEmailAndPassword(email, password)	
-		.then(function (user) {
-			console.log(user);
-
-			location.href = "dashboard.html";
-		})
-		.catch(function (err) {
-			console.log(err);
-		});
-
-});
-
-// Sign the logged in user out
-$("#signout").on('click', function () {
-
-	auth.signOut() // firebase method to sign otu
-		.then(function () {
-
-			location.href = 'signin.html'; // redirect the user to the sign in page
-			
-		})
-		// Dealing with errors
-		.catch(function (err) {
-			console.log(err);
-		})
-
-});
-
-// Dashboard section
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-   
-   	// Refering the Firebase Section
-    var refLanguage = database.ref("users/" + user.uid + "/languages"),
-    	ref = database.ref("users/" + user.uid),
-    	allUsers = database.ref("users");
-
-
-    // Get the current logged in user info
-    ref.on('value', function (user) {
-
-    	var currentUser = user.val(); // info
-
-    	$("#fullname").html("Welcome back, dear " +  currentUser.firstname + " " + currentUser.lastname);
-    	$("#email").html(currentUser.email);
+        usersContainer.append(title, img);
+        $(".container--users").append(usersContainer);
 
     });
+}
 
-    // Adding languages to the Database
-    $("#addLanguage").on('click', function (event) {
-    	
-    	event.preventDefault(); // preventing the browser from refreshing
 
-    	var language = $("#language").val().trim();
+for(var i = 0; i < users.length; i++){
 
-    	var userLanguage = {
+    getData(streamEndpoint, users[i], function (streamer) {
+            
+        if(streamer.stream !== null){
+            
+            var status = $("<p>");
+                status.html("Online");
 
-    		language:language
-    	}
+            var url = $("<a>");
+                url.attr({
+                    "href": streamer.stream.channel.url,
+                    "target":"_blank"
+                });
+                url.html(streamer.stream.channel.url);  
 
-    	refLanguage.set(userLanguage); // firebase reference
+            var followers = $("<p>");  
+                followers.html("Followers: " + streamer.stream.channel.followers); 
 
+            var game = $("<p>");
+                game.html("Game: " + streamer.stream.game); 
+
+            var viewers = $("<p>");
+                viewers.html("Viewers: " + streamer.stream.viewers);         
+
+            $("#" + streamer.stream.channel._id).removeClass("offline");
+
+            $('#' + streamer.stream.channel._id).append(url, status, followers, game, viewers);
+            $('#' + streamer.stream.channel._id).addClass("online");
+            
+        } 
+
+        else{
+            
+            $(".container--offline").remove();
+
+            var status = $("<div>");
+                status.addClass("container--offline");
+
+            var offline = $("<p>");
+                offline.html("Offline");
+
+            status.append(offline);    
+            $(".offline").append(status)            
+        }  
     });
+}
 
 
-    // Getting the languages values
-    refLanguage.on("value", function (languages) {
+$("#all").on('click', function () {
+    $(".offline, .online").css('display', 'block');
+});
 
-    	var currentUserLanguage = languages.val();
+$("#online").on('click', function () {
+    $(".online").css('display', 'block');
+    $(".offline").css('display', 'none');
+});
 
-    	// Output to the DOM
-    	$("#language").val(currentUserLanguage.language);
-    	$("#display").html(currentUserLanguage.language);
-    });
-
-
-
-    // Display all users
-    allUsers.on('value', function (all) {
-    	var allCurrentUser = all.val();
-
-    	console.log(allCurrentUser);
-
-    	var displayAllUsers = [];
-
-
-    	// Looping through users ID
-    	for(var user in allCurrentUser){
-    		displayAllUsers.push("User: " + allCurrentUser[user].firstname + ", ");
-    		
-    	}
-
-    	// Looping through all users
-    	for(var i = 0; i < displayAllUsers.length; i++){
-    		var li = $("<li>");
-    			li.html(displayAllUsers[i]);
-
-    		$("#users").append(li);	// appending them to the ul tag in the HTML
-    	}
-    });
-
-  } else {
-
-   	console.log("No user found!!!");
-
-  }
+$("#offline").on('click', function () {
+    $(".online").css('display', 'none');
+    $(".offline").css('display', 'block'); 
 });
 
 
-// Note: 
-// The way, we are displaying all the users, is not recommended for security reasons
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
